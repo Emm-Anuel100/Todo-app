7be90115bdf92e82fa84d90edce780bb82c770cc
Chinweokwu Emmanuel <125787874+Emm-Anuel100@users.noreply.github.com>
2023-06-25 18:51:33 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Todo_application
+{
+    public partial class Form1 : Form
+    {
+
+        // list to hold all of the todos
+       static List<String> names = new List<string>();
+
+        // BindingSource to connect list items in textbox2 to the listbox in form1
+        BindingSource todosBindingSource = new BindingSource();
+
+
+        internal void receiveData(string newtodo)
+        {
+            names.Add(newtodo); // newtodo base on the parameter in this method
+
+            MessageBox.Show("you have " + names.Count + " todos left in the list.");
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // adding todos in the list of todos
+
+            //names.Add("afsn");
+            //names.Add("afsn");
+            //names.Add("afsn");
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked_1(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.google.com");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+            // let the dataSource be comming from names
+            todosBindingSource.DataSource = names;
+
+            // item that should be in listbox should come from todoBindingSource
+            listBox1.DataSource = todosBindingSource;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 Check = new Form2();
+            Check.Show();
+            this.Hide();
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            // actions occur when this form regains focus. when it is displayed in front
+
+            todosBindingSource.ResetBindings(false);
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // this code is responsible for the sorting of the todos
+
+            if (comboBox1.SelectedItem == "A - Z")
+            {
+                names.Sort();
+             
+            }
+            else
+            {
+                names.Sort();
+                names.Reverse();
+            }
+            todosBindingSource.ResetBindings(false);
+        }
+
+
+        private void listBox1_Click(object sender, EventArgs e) // when clicked on an item in the listbox
+        {
+
+            int i = listBox1.SelectedIndex;
+         
+            if (i >= 0)
+            {
+                DialogResult result = MessageBox.Show("would you like to delete todo " + names[i] + " ", "comfirm", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    names.RemoveAt(i);
+                    todosBindingSource.ResetBindings(false);
+
+                    MessageBox.Show("you have " + names.Count + " todos left in the list."); // display todos left
+                }
+            }
+
+        }
+    }
+}
---
+++ Form2.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Todo_application
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 31);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(441, 36);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(283, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial Narrow", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(324, 143);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(53, 36);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(465, 209);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(481, 248);
+            this.Name = "Form2";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add Todos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
